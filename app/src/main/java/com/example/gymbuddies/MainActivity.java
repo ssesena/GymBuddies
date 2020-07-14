d1e4bc2844ae23adb0f90da8a1d8083e0243cffc
Samuel Sesena <samuelsesena@fb.com>
2020-07-14 12:44:51 -0700

    Added Tab Navigation

--- app/src/main/java/com/example/gymbuddies/MainActivity.java
+++ app/src/main/java/com/example/gymbuddies/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.gymbuddies;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.gymbuddies.databinding.ActivityMainBinding;
+import com.example.gymbuddies.fragments.ChatFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    final FragmentManager fragmentManager = getSupportFragmentManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.bottomNavigation.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_profile:
+                        fragment = new ChatFragment();
+                        break;
+                    case R.id.action_home:
+                        fragment = new ChatFragment();
+                        break;
+                    case R.id.action_chat:
+                    default:
+                        fragment = new ChatFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+        binding.bottomNavigation.setSelectedItemId(R.id.action_home);
     }
 }
