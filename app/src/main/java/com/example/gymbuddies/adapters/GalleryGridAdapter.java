4447bf2790a63b55288ee425ef10757f3200c295
Samuel Sesena <samuelsesena@fb.com>
2020-07-15 17:15:06 -0700

    Added images for cells in GridView

--- app/src/main/java/com/example/gymbuddies/adapters/GalleryGridAdapter.java
+++ app/src/main/java/com/example/gymbuddies/adapters/GalleryGridAdapter.java
@@ -2,6 +2,7 @@ package com.example.gymbuddies.adapters;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
@@ -16,16 +17,18 @@ import java.util.List;
 public class GalleryGridAdapter extends BaseAdapter {
     public static final String TAG = "GalleryGridAdapter";
     private Context context;
+    public static final int HEIGHT = 400;
+    public static final int NUMBER_OF_GALLERY_PHOTOS = 6;
+
     private List<String> images = new ArrayList<String>();
 
     public GalleryGridAdapter(Context context) {
         this.context = context;
-
     }
 
     @Override
     public int getCount() {
-        return 6;
+        return NUMBER_OF_GALLERY_PHOTOS;
     }
 
     @Override
@@ -41,17 +44,17 @@ public class GalleryGridAdapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         ImageView imageView;
-        Log.i(TAG, "method is called");
         if(view == null){
+            view = LayoutInflater.from(context).inflate(R.layout.item_gallery_photo,null);
             Log.i(TAG, "view is null");
-            imageView = new ImageView(context);
-            imageView.setLayoutParams(new GridView.LayoutParams(100,100));
-            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
-        } else{
-            imageView = (ImageView) view;
         }
+        view.setLayoutParams(new GridView.LayoutParams(GridView.AUTO_FIT, HEIGHT));
+        ImageView icon = view.findViewById(R.id.ivRemovePhoto);
+        icon.setImageResource(R.drawable.ic_baseline_remove_circle_24);
+        imageView = view.findViewById(R.id.ivGalleryImage);
+        //imageView.setImageResource(R.drawable.ic_baseline_image_24);
 
-        imageView.setImageResource(R.drawable.ic_baseline_image_24);
-        return imageView;
+        //imageView.setImageResource(R.drawable.ic_baseline_image_24);
+        return view;
     }
 }
